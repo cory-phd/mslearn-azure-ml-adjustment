d7be06bbfaa34cecf6b48a2d5ae1e07867c61298
cory-phd <cory-ash-2024-phd@outlook.com>
2024-07-01 21:19:19 -0400

    Update setup.sh
    Changed compute and cluster to STANDARD_EDV5.

--- Labs/11/setup.sh
+++ Labs/11/setup.sh
@@ -29,8 +29,8 @@ az configure --defaults workspace=$WORKSPACE_NAME
 
 # Create compute instance
 echo "Creating a compute instance with name: " $COMPUTE_INSTANCE
-az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_D4DS_V4 --type ComputeInstance 
+az ml compute create --name ${COMPUTE_INSTANCE} --size STANDARD_EDV5 --type ComputeInstance 
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_D4DS_V4 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_EDV5 --max-instances 2 --type AmlCompute 
